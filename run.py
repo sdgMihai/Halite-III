1fcdba9ddc155593621f918a88e53aac37422d89
Craciunoiu Cezar <craciunoiu.cezar@yahoo.com>
2019-04-09 12:53:43 +0300

    Added Etapa2

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ class HaliteEnv(object):
     def __add_bots(self, cmd):
 
         # Specify the number of bots and how to run them
-        cmd += " -n {} ".format(len(self.bots))
+        # cmd += " -n {} ".format(len(self.bots))
         for bot in self.bots:
             cmd = self.__add_bot(cmd, bot)
         return cmd
@@ -207,7 +207,76 @@ def round_one(cmd, map):
 def round_two(cmd, map):
 
     sys.stdout.write("Round 2 - 1vs1 battles!\n")
-    sys.stdout.write("Coming soon!\n")
+
+    env = HaliteEnv(cmd)
+
+    games = [
+        (32, 32, 20596, "./bots/Odysseus"),
+        (32, 32, 75273, "./bots/Dragon"),
+        (32, 32, 58900, "./bots/Joker"),
+        (40, 40, 93689, "./bots/Odysseus"),
+        (40, 40, 98091, "./bots/Dragon"),
+        (56, 56, 1234, "./bots/Odysseus"),
+        (56, 56, 42, "./bots/Odysseus"),
+        (56, 56, 1024, "./bots/Dragon"),
+    ]
+
+    max_score = 0.45             # Round score
+    game_weight = max_score / 5  # For max score you need to win on 5/8 maps
+    player_score = 0.0
+    battles_won = 0
+    game_scores = []
+
+    if map != -1:
+        games = games[map:map + 1]
+
+    for idx, game in enumerate(games):
+
+        width, height, seed, bot = game
+        env.bots = [cmd, bot]
+
+        env.height = height
+        env.width  = width
+        env.seed   = seed
+        points     = 0.0
+
+        binary_log, text_log = env.run()
+
+        if text_log is None:
+            sys.stdout.write("Map {} score: {}\n".format(idx, points))
+            continue
+        else:
+
+            with open(text_log, "r") as f:
+
+                result = json.loads(f.read())
+
+                if result["stats"]["0"]["rank"] == 1:
+                    points = game_weight
+                    sys.stdout.write("You've won!\n")
+                elif result["stats"]["0"]["score"] >= 0.9 * result["stats"]["1"]["score"]:
+                    points = game_weight * 0.9
+                    sys.stdout.write("You've lost.. but you were very close!\n")
+                else:
+                    points = 0.0
+                    sys.stdout.write("You've lost!\n")
+
+                sys.stdout.write("Map score: {}\n".format(points))
+                game_scores.append(points)
+                player_score += points
+
+        call(["mv {} ./replays/replay-map-{}.hlt".format(binary_log, idx)], shell=True)
+        call(["mv {} ./replays-readable/replay-map-{}.hlt".format(text_log, idx)], shell=True)
+
+    final_score = round(min(player_score, max_score), 2)
+
+    sys.stdout.write("Round 2 - done!\nFinal score: {}/{}\n".format(final_score,
+                                                                    max_score))
+
+    with open("result.json", "w") as f:
+        result = {"final_score": final_score}
+        json.dump(result, f)
+
     return
 
 
@@ -228,7 +297,7 @@ def main():
 
     parser = argparse.ArgumentParser(description='PA project evaluator')
     parser.add_argument('--cmd', required=True, help="Command line instruction to execute the bot. eg: ./MyBot")
-    parser.add_argument('--round', type=int, default=1, help="Round index (1, 2, or 3), default 3")
+    parser.add_argument('--round', type=int, default=2, help="Round index (1, 2, or 3), default 2")
     parser.add_argument('--map', type=int, default=-1, help="Specify a specific map to play for the current round")
     parser.add_argument('--clean_logs', action="store_true", help="Remove logs/game results, call make clean")
     parser.add_argument('--clean', action="store_true", help="Call make clean before make when building player sources")
